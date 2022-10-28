a4b87bf78136bfd1389d0c2a0bd7110da049008d
victorv47 <victor.vela94@gmail.com>
2022-10-27 20:47:50 -0500

    Added Questions

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -9,13 +9,15 @@
 </template>
 
 <style lang="postcss" scoped>
-.brand {
-  @apply flex items-center justify-center gap-4;
-  & :slotted(img) {
-    @apply h-16 w-16;
+  .brand {
+    @apply flex items-center justify-center gap-4;
+
+    & :slotted(img) {
+      @apply h-16 w-16;
+    }
+
+    & .title {
+      @apply text-center text-6xl font-thin uppercase tracking-widest text-slate-700;
+    }
   }
-  & .title {
-    @apply text-6xl font-thin uppercase tracking-widest text-slate-700;
-  }
-}
 </style>
