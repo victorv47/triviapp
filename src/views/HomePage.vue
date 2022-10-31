4848a471f875e120e0225008f4d68054355def83
victorv47 <victor.vela94@gmail.com>
2022-10-30 20:18:25 -0500

    Added Category Once Call

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,24 +1,24 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import useAPI from '@/composables/useAPI'
-import BaseTitle from '@/components/BaseTitle.vue'
+  import { onMounted } from 'vue'
+  import useAPI from '@/composables/useAPI'
+  import BaseTitle from '@/components/BaseTitle.vue'
 
-const api = useAPI()
-const categories = ref([])
+  const { categories, getCategories } = useAPI()
 
-onMounted(async () => {
-  categories.value = await api.getCategories()
-})
+  onMounted(async () => {
+    await getCategories()
+  })
 </script>
 
 <template>
+  <!--  -->
   <BaseTitle>
     <template #logo>
       <img src="logo.svg" alt="logo" />
     </template>
     Triviapp
   </BaseTitle>
-  <div class="categories">
+  <div v-if="categories.length > 0" class="categories">
     <RouterLink
       v-for="category in categories"
       :key="category.id"
@@ -29,16 +29,14 @@ onMounted(async () => {
     </RouterLink>
   </div>
 </template>
-
 <style lang="postcss" scoped>
-.categories {
-  @apply grid flex-grow grid-cols-4 gap-12;
-  & .category {
-    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
-
-    &:hover {
-      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+  .categories {
+    @apply grid flex-grow grid-cols-4 gap-12;
+    & .category {
+      @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+      &:hover {
+        @apply cursor-pointer border-red-500 bg-red-500 text-white;
+      }
     }
   }
-}
 </style>
